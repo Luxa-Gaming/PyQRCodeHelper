c30d3e6aeb0c4aced7386ad2bed9077b2960b28a
Luxa <luxaxy@gmx.de>
2021-04-03 18:23:51 +0200

    + + deleted functions

--- main.py
+++ main.py
@@ -1,16 +1,11 @@
 # Generate
 import pyqrcode
+qr = pyqrcode.create("", error="H")
+qr.png("qrcode.png", module_color=(0, 255, 0, 255), background=(0, 0, 0, 255), scale=5)
 
 # Decode
 from PIL import Image
 from pyzbar.pyzbar import decode
 
-
-def generate(url, name):
-    qr = pyqrcode.create(url, error="H")
-    qr.png(name, module_color=(0, 255, 0, 255), background=(0, 0, 0, 255), scale=5)
-
-
-def decode(image):
-    data = decode(Image.open(image))
-    print(data)
+data = decode(Image.open(""))
+print(data)
